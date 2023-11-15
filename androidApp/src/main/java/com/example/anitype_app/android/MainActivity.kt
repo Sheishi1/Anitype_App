569f7d91873283b6b42877b24b65da70309f9244
Sheishi1 <grikerbek1@mail.ru>
2023-11-15 23:58:45 +0500

    2

--- androidApp/src/main/java/com/example/anitype_app/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/anitype_app/android/MainActivity.kt
@@ -4,9 +4,12 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,10 +17,16 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.BiasAlignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -46,84 +55,169 @@ class MainActivity : ComponentActivity() {
 @SuppressLint("InvalidColorHexValue")
 @Composable
 fun MyApp(modifier: Modifier) {
-    Column {
-        TrailerPreview()
+    LazyColumn {
+        item {
+            TrailerPreview()
+            ListsPreview()
+        }
     }
 }
 
 @Composable
-fun trailer(image: String, modifier: Modifier) {
-    Surface(
-        modifier
-            .height(400.dp),
-
+fun trailer(image: String, title: String, rate: String, series: String, genres: String, modifier: Modifier) {
+    BoxWithConstraints(
+        modifier.height(400.dp),
+        contentAlignment = Alignment.BottomCenter,
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.zerotwo),
+            contentDescription = "trailers of any anime",
+            contentScale = ContentScale.Crop,
+            modifier = modifier
+                .fillMaxSize()
+                .heightIn(300.dp, 500.dp)
+        )
+        // Затемнение снизу trailer
+        Canvas(modifier = modifier.fillMaxSize(),
+            onDraw = { drawRect(Brush.verticalGradient(listOf(Color.Transparent, Color.Black)))
+            })
+        Column (
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
         ) {
-        Box(modifier = modifier) {
-
-            Image(
-                painter = painterResource(id = R.drawable.zerotwo),
-                contentDescription = "trailers of aby anime",
-                contentScale = ContentScale.Crop,
-                modifier = modifier
-                    .fillMaxSize()
-                    .heightIn(300.dp, 500.dp)
-            )
             Surface(
-                color = Color.Black,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .heightIn(80.dp, 100.dp)
-                    .background(Color.Black)
-                    .align(Alignment.BottomCenter)
+                color = Color.Transparent,
             ) {
-                Surface(
-                    color = Color.Transparent,
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.SpaceBetween,
                 ) {
-                    Column(
-                        horizontalAlignment = Alignment.CenterHorizontally,
+                    Text(
+                        text = title,
+                        color = Color.White,
+                        textAlign = TextAlign.Center,
+                        fontWeight = FontWeight.ExtraBold,
+                        fontFamily = FontFamily(Font(R.font.rubik)),
+                        fontSize = 24.sp,
+                    )
+
+                    Row(
+
                     ) {
-                        Text(
-                            text = "Любимый во франксе",
-                            color = Color.White,
-                            textAlign = TextAlign.Center,
-                            fontWeight = FontWeight.W700,
-                            fontFamily = FontFamily(Font(R.font.rubik)),
-                            fontSize = 24.sp,
-                        )
+                    Text(
+                        modifier = Modifier.padding(end = 8.dp),
+                        text = rate,
+                        color = Color.Green,
+                        fontWeight = FontWeight.W400,
+                        fontFamily = FontFamily(Font(R.font.rubik)),
+                        fontSize = 10.sp,
+                    )
 
-                        Row(
+                    Text(
+                        modifier = Modifier.padding(end = 8.dp),
+                        text = series,
+                        color = Color.White,
+                        fontWeight = FontWeight.W400,
+                        fontFamily = FontFamily(Font(R.font.rubik)),
+                        fontSize = 10.sp,
+                    )
 
-                        ) {
-                            Text(
-                                modifier = Modifier.padding(end = 8.dp),
-                                text = "9.99",
-                                color = Color.Green,
-                                fontWeight = FontWeight.W400,
-                                fontFamily = FontFamily(Font(R.font.rubik)),
-                                fontSize = 10.sp,
-                            )
-
-                            Text(
-                                modifier = Modifier.padding(end = 8.dp),
-                                text = "24 эпизода",
-                                color = Color.White,
-                                fontWeight = FontWeight.W400,
-                                fontFamily = FontFamily(Font(R.font.rubik)),
-                                fontSize = 10.sp,
-                            )
-
-                            Text(
-                                modifier = Modifier.padding(end = 8.dp),
-                                text = "Драма, Меха, Романтика",
-                                color = Color.White,
-                                fontWeight = FontWeight.W400,
-                                fontFamily = FontFamily(Font(R.font.rubik)),
-                                fontSize = 10.sp,
-                            )
-                        }
-                    }
+                    Text(
+                        modifier = Modifier.padding(end = 8.dp),
+                        text = genres,
+                        color = Color.White,
+                        fontWeight = FontWeight.W400,
+                        fontFamily = FontFamily(Font(R.font.rubik)),
+                        fontSize = 10.sp,
+                    )
+                }
                 }
             }
+            Button(
+                onClick = { /*TODO*/ },
+                shape = RoundedCornerShape(20.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = Color.White),
+            ) {
+                Text(
+                    text = "Смотреть",
+                    color = Color.Black,
+                    fontSize = 16.sp,
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun lists(name: String, modifier: Modifier) {
+    BoxWithConstraints(
+        modifier
+            .fillMaxSize()
+            .padding(30.dp)
+            .border(2.dp, Color.Black, shape = RoundedCornerShape(topStart = 8.dp, topEnd = 10.dp))
+    ) {
+        Column(
+            verticalArrangement = Arrangement.SpaceBetween,
+        ) {
+            Text(
+                modifier = Modifier.padding(top = 20.dp, bottom = 20.dp),
+                text = "Продолжить просмотр",
+                color = Color.White,
+                fontWeight = FontWeight.W700,
+                fontFamily = FontFamily(Font(R.font.rubik)),
+                fontSize = 16.sp,
+            )
+            LazyRow (
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                items(10) { listItem(name = name, modifier = modifier) }
+            }
+
+            Text(
+                modifier = Modifier.padding(top = 20.dp, bottom = 20.dp),
+                text = "Новинки",
+                color = Color.White,
+                fontWeight = FontWeight.W700,
+                fontFamily = FontFamily(Font(R.font.rubik)),
+                fontSize = 16.sp,
+            )
+            LazyRow(
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                items(10) { listItem(name = name, modifier = modifier) }
+            }
+        }
+    }
+}
+
+@Composable
+fun listItem (name: String, modifier: Modifier) {
+    Surface (
+        modifier
+            .height(200.dp)
+            .width(250.dp)
+            .padding(end = 10.dp),
+        color = Color.Transparent,
+        ) {
+        Column {
+            Image(
+                painter = painterResource(id = R.drawable.zerotwo),
+                contentDescription = "listItem",
+                contentScale = ContentScale.Crop,
+                modifier = modifier
+                    .height(150.dp)
+                    .width(250.dp)
+            )
+
+            Text(
+                text = name,
+                color = Color.White,
+                fontWeight = FontWeight.W700,
+                fontFamily = FontFamily(Font(R.font.rubik)),
+                fontSize = 16.sp
+            )
         }
     }
 }
@@ -132,6 +226,26 @@ fun trailer(image: String, modifier: Modifier) {
 @Composable
 fun TrailerPreview() {
     MyApplicationTheme {
-        trailer("zxc", Modifier)
+        trailer(image = "any",
+            title = "Милый во франксе",
+            rate = "9.99",
+            series = "24 эпизода",
+            genres = "Драма, Меха, Романтика",
+            Modifier)
+    }
+}
+
+@Preview
+@Composable
+fun ListsPreview() {
+    MyApplicationTheme {
+        lists(name = "Милый во франксе", modifier = Modifier)
+    }
+}
+@Preview
+@Composable
+fun ListItemPreview() {
+    MyApplicationTheme {
+        listItem(name = "Милый во франксе", modifier = Modifier)
     }
 }
